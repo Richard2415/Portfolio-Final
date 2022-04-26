6970b522f09506fd09c92e3f0df6e671a7625d63
Richard <85327997+Richard2415@users.noreply.github.com>
2022-04-26 12:29:42 +0530

    Update helperList.js

--- src/utils/helperList.js
+++ src/utils/helperList.js
@@ -131,7 +131,7 @@ export const projectsList = [{
 {
   label: 'Chatter',
   imgSrc: 'static/images/chatterimg.png',
-  src: 'https://chatterapp-b268824.firebaseapp.com/',
+  src: 'https://chatter-beb4b.web.app/',
   visit: 'https://github.com/Richard2415/Chatter-app',
   description: 'Chatter is simply a instant messaging app it can be used to communicate with friends in around world using the secured link address and hence to store and messages and share instant messages used Firebase as a realtime database .',
   stack: [{
